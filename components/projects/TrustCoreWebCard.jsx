643ca488e822338df5d1bf75ec0266b32a3077c9
emma1110 <emma1110@users.noreply.github.com>
2026-07-22 18:22:39 +0700

    Remove black corners from TrustCore Web image

--- components/projects/TrustCoreWebCard.jsx
+++ components/projects/TrustCoreWebCard.jsx
@@ -43,9 +43,9 @@ export default function TrustCoreWebCard() {
         <div className="mt-[16px] flex min-h-0 w-full flex-1 items-end justify-center px-4 sm:mt-[24px] sm:px-[20px] lg:mt-[40px] lg:px-0">
           <div className="flex h-full min-h-0 w-full items-end overflow-hidden lg:w-[94%]">
             <img
-              src="/images/tc page.avif"
+              src="/images/tc-page.webp"
               alt="TrustCore Landing"
-              className="block h-full min-h-0 w-full scale-[1.004] object-contain object-bottom"
+              className="block h-full min-h-0 w-full object-contain object-bottom"
               width="1400"
               height="858"
               loading="lazy"
